e835cb8dd557ab025ec900d73db9edb3ef3c7ed2
lay <lanyu@csnet.lan>
2019-07-08 11:25:25 +0800

    test cwe116

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, make_response
 from flask import render_template
 
 app = Flask(__name__)
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/unsafe')
+def unsafe():
+    first_name = request.args.get('name')
+    return str(first_name)
 
 if __name__ == "__main__":
     app.run(debug=True)
